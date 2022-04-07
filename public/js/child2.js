28d650d0d966ca5477e27d30fea92a7b75fe24a7
S Snehalatha <55445142+sneha096@users.noreply.github.com>
2022-04-07 13:53:41 +0530

    Delete public directory

--- public/js/child2.js
+++
@@ -1,5 +0,0 @@
-var tz = require('C:/Users/snehalatha/Desktop/child process/timeZone.js');
-
-process.on('message', (cityName)=>{
-        process.send(JSON.parse(JSON.stringify(tz.timeForOneCity(cityName))));
-});
--- public/js/child3.js
+++
@@ -1,5 +0,0 @@
-var tz = require('C:/Users/snehalatha/Desktop/child process/timeZone.js');
-
-process.on('message', (body)=>{
-    process.send(JSON.parse(JSON.stringify(tz.nextNhoursWeather(body.city_Date_Time_Name, body.hours, tz.allTimeZones()))));
-});
